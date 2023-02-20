badac800d6292ae7bc57f220d65db7ceb7d2a7a2
primocodes <primocodes@gmail.com>
2023-02-20 15:21:55 +0100

    hello there

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ * Description: Prints "a
+ * Return: Always return 0
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
